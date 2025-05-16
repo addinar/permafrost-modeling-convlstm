dca525be930c6e9ac18911d912ef0f9b375eb540
addinar <raddina24@gmail.com>
2025-05-16 15:47:24 -0400

    retrained model

--- model/models/convlstm.py
+++ model/models/convlstm.py
@@ -5,7 +5,7 @@ class ConvLSTM(nn.Module):
     def __init__(self, input_features, conv_out_channels=64, lstm_hidden=64, output_features=4):
         super(ConvLSTM, self).__init__()
         self.conv1 = nn.Conv1d(in_channels=input_features, out_channels=conv_out_channels, kernel_size=7, stride=1, padding=3)
-        self.batch_norm = nn.BatchNorm1d(conv_out_channels)
+        self.norm = nn.LayerNorm(conv_out_channels)
         self.lstm = nn.LSTM(input_size=conv_out_channels, hidden_size=lstm_hidden, batch_first=True)
         self.fc = nn.Linear(lstm_hidden, output_features)
         self.h0 = nn.Parameter(torch.zeros(1, lstm_hidden))
@@ -14,8 +14,8 @@ class ConvLSTM(nn.Module):
     def forward(self, x):
         x = x.permute(0, 2, 1)    # [batch_size, features, seq_length]
         x = self.conv1(x)          # [batch_size, conv_out_channels, seq_length] 
-        x = self.batch_norm(x)     # Apply BatchNorm
-        x = x.permute(0, 2, 1)     # [batch_size, seq_length//2, conv_out_channels]
+        x = x.permute(0, 2, 1)
+        x = self.norm(x)
 
         batch_size = x.size(0)
         h0 = self.h0.expand(1, batch_size, -1).contiguous()
